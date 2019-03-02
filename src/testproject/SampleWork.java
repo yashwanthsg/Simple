01d252037494413b27a3d640806bd4e0e362ad1c
yashwanthsg <yashsg94@gmail.com>
2019-03-02 14:26:08 +0530

    commited

---
+++ src/testproject/SampleWork.java
@@ -0,0 +1,16 @@
+package testproject;
+
+public class SampleWork {
+	
+	public void javaCourse() {
+		
+		System.out.println("Java Course");
+		
+	}
+
+	public static void main(String[] args) {
+		SampleWork sd = new SampleWork();
+		sd.javaCourse();
+	}
+	
+}
